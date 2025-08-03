e260525f2dced1387585a2632f4eae65003ea319
Cerek Hillen <cerekh@gmail.com>
2025-08-03 16:12:15 -0400

    implement the go version

---
+++ main.go
@@ -0,0 +1,191 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type Form struct {
+	gorm.Model
+
+	ID        string `gorm:"primaryKey"`
+	Prompt    string
+	Responses []Response `gorm:"foreignKey:FormID"`
+}
+
+type Response struct {
+	gorm.Model
+
+	ID     string `gorm:"primaryKey"`
+	FormID string
+	Text   string
+}
+
+func main() {
+	if err := mainImpl(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func mainImpl() error {
+	db, err := gorm.Open(sqlite.Open("form.db"), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+	if err := db.AutoMigrate(&Form{}, &Response{}); err != nil {
+		return err
+	}
+
+	engine := gin.Default()
+
+	engine.POST("/api/v1/form", createForm(db))
+	engine.GET("/api/v1/form/:id", getForm(db))
+	engine.POST("/api/v1/form/:id/response", postResponse(db))
+
+	log.Print("Listening on 127.0.0.1:8000")
+	engine.Run("127.0.0.1:8000")
+	return nil
+}
+
+type CreateFormRequest struct {
+	Prompt string `json:"prompt"`
+}
+
+type CreateFormResponse struct {
+	ID     string `json:"id"`
+	Prompt string `json:"prompt"`
+}
+
+func createForm(db *gorm.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		req := CreateFormRequest{}
+		if err := c.BindJSON(&req); err != nil {
+			c.Error(err)
+			return
+		}
+
+		id, err := uuid.NewV7()
+		if err != nil {
+			c.Error(err)
+			return
+		}
+
+		form := Form{
+			ID:     id.String(),
+			Prompt: req.Prompt,
+		}
+		if err := gorm.G[Form](db).Create(c.Request.Context(), &form); err != nil {
+			c.Error(err)
+			return
+		}
+
+		c.JSON(200, CreateFormResponse{ID: form.ID, Prompt: form.Prompt})
+	}
+}
+
+type GetFormRequest struct {
+	ID string `json:"id"`
+}
+
+type FormResponse struct {
+	ID        string              `json:"id"`
+	Prompt    string              `json:"prompt"`
+	Responses []FormResponseEntry `json:"responses"`
+}
+
+type FormResponseEntry struct {
+	ID   string `json:"id"`
+	Text string `json:"text"`
+}
+
+func getForm(db *gorm.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id := c.Params[0].Value
+		form, err := gorm.G[Form](db).Where("id = ?", id).Preload("Responses", func(db gorm.PreloadBuilder) error {
+			return nil
+		}).First(c.Request.Context())
+		if err != nil {
+			c.Error(err)
+			return
+		}
+
+		responses := make([]FormResponseEntry, len(form.Responses))
+		for i, response := range form.Responses {
+			responses[i] = FormResponseEntry{
+				ID:   response.ID,
+				Text: response.Text,
+			}
+		}
+
+		c.JSON(
+			200,
+			FormResponse{
+				ID:        form.ID,
+				Prompt:    form.Prompt,
+				Responses: responses,
+			},
+		)
+	}
+}
+
+type PostResponseRequest struct {
+	Text string
+}
+
+func postResponse(db *gorm.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		req := PostResponseRequest{}
+		if err := c.BindJSON(&req); err != nil {
+			c.Error(err)
+			return
+		}
+
+		id := c.Params[0].Value
+		form, err := gorm.G[Form](db).Where("id = ?", id).Preload("Responses", func(db gorm.PreloadBuilder) error {
+			return nil
+		}).First(c.Request.Context())
+		if err != nil {
+			c.Error(err)
+			return
+		}
+
+		uuid, err := uuid.NewV7()
+		if err != nil {
+			c.Error(err)
+			return
+		}
+
+		newResponse := Response{
+			ID:     uuid.String(),
+			FormID: id,
+			Text:   req.Text,
+		}
+
+		if err := gorm.G[Response](db).Create(c.Request.Context(), &newResponse); err != nil {
+			c.Error(err)
+			return
+		}
+
+		form.Responses = append(form.Responses, newResponse)
+		responses := make([]FormResponseEntry, len(form.Responses))
+		for i, response := range form.Responses {
+			responses[i] = FormResponseEntry{
+				ID:   response.ID,
+				Text: response.Text,
+			}
+		}
+
+		c.JSON(
+			200,
+			FormResponse{
+				ID:        form.ID,
+				Prompt:    form.Prompt,
+				Responses: responses,
+			},
+		)
+	}
+}
--- src/form.gleam
+++ src/form.gleam
@@ -1,9 +1,13 @@
+import gleam/bool
 import gleam/dynamic/decode
 import gleam/erlang/process
 import gleam/http
 import gleam/io
+import gleam/json
+import gleam/list
 import mist
 import sqlight
+import uuid
 import wisp
 import wisp/wisp_mist
 
@@ -37,7 +41,6 @@ pub fn main() -> Nil {
     |> mist.port(8000)
     |> mist.start
 
-  io.println("Listening on :8000")
   process.sleep_forever()
 }
 
@@ -68,35 +71,76 @@ fn handle_create_room(
   conn: sqlight.Connection,
   req: wisp.Request,
 ) -> wisp.Response {
-  use json <- wisp.require_json(req)
-
   let decoder = {
     use prompt <- decode.field("prompt", decode.string)
     decode.success(prompt)
   }
-  use prompt <- decode.run(json, decoder)
+  use prompt <- require_decoded_json(req, decoder)
 
-  let decoder = {
-    use id <- decode.field(0, decode.string)
-    use prompt <- decode.field(1, decode.string)
-    decode.success(#(id, prompt))
-  }
-  let _ =
-    sqlight.query(
-      "INSERT INTO forms (prompt) RETURNING (id, prompt);",
-      conn,
-      [prompt],
-      decoder,
-    )
-  todo
+  let form_id = uuid.v7_string()
+  use _ <- internal_server_error(sqlight.query(
+    "INSERT INTO forms (id, prompt) VALUES (?, ?) RETURNING (id);",
+    conn,
+    [sqlight.text(form_id), sqlight.text(prompt)],
+    decode.success(fn(a) { a }),
+  ))
+
+  wisp.json_response(
+    json.to_string_tree(json.object([#("new_form_id", json.string(form_id))])),
+    200,
+  )
 }
 
 fn handle_get_room(
   conn: sqlight.Connection,
-  req: wisp.Request,
-  id: string,
+  _req: wisp.Request,
+  id: String,
 ) -> wisp.Response {
-  todo
+  let decoder = {
+    use form_prompt <- decode.field(0, decode.string)
+    decode.success(form_prompt)
+  }
+  use form <- internal_server_error(sqlight.query(
+    "SELECT prompt FROM forms WHERE id = ?;",
+    conn,
+    [sqlight.text(id)],
+    decoder,
+  ))
+  let assert [form_prompt] = form
+  use <- bool.guard(list.length(form) > 0, wisp.not_found())
+
+  let decoder = {
+    use response_id <- decode.field(0, decode.string)
+    use response_text <- decode.field(1, decode.string)
+    decode.success(#(response_id, response_text))
+  }
+  use responses <- internal_server_error(sqlight.query(
+    "SELECT id, response FROM responses WHERE form_id = ?",
+    conn,
+    [sqlight.text(id)],
+    decoder,
+  ))
+
+  wisp.json_response(
+    json.to_string_tree(
+      json.object([
+        #("prompt", json.string(form_prompt)),
+        #(
+          "responses",
+          json.preprocessed_array(
+            list.map(responses, fn(response) {
+              let #(response_id, response_text) = response
+              json.object([
+                #("id", json.string(response_id)),
+                #("text", json.string(response_text)),
+              ])
+            }),
+          ),
+        ),
+      ]),
+    ),
+    200,
+  )
 }
 
 fn handle_post_response(
@@ -105,4 +149,29 @@ fn handle_post_response(
   id: string,
 ) -> wisp.Response {
   todo
+}
+
+fn require_decoded_json(
+  req: wisp.Request,
+  decoder: decode.Decoder(t),
+  next: fn(t) -> wisp.Response,
+) -> wisp.Response {
+  use json <- wisp.require_json(req)
+  case decode.run(json, decoder) {
+    Ok(t) -> next(t)
+    Error(_) -> wisp.bad_request()
+  }
+}
+
+fn internal_server_error(
+  value: Result(t, u),
+  next: fn(t) -> wisp.Response,
+) -> wisp.Response {
+  case value {
+    Ok(t) -> next(t)
+    Error(e) -> {
+      echo e
+      wisp.internal_server_error()
+    }
+  }
 }
