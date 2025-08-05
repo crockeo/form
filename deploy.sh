a749b48d5b4cf62c06ddaee1f773a437d5f111c4
Cerek Hillen <cerekh@gmail.com>
2025-08-05 10:59:24 -0700

    make deploy work, fix title

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,9 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+echo "--- Building + Copying frontend"
 bun run build
-scp -r dist crockeo@homeserver.crockeo.net:~/form
+scp -r dist crockeo@homeserver.crockeo.net:~/form_fe
+
+echo "--- Executing remote deploy"
 ssh crockeo@homeserver.crockeo.net "bash -s" < ./deploy_remote.sh
--- deploy_remote.sh
+++ deploy_remote.sh
@@ -4,6 +4,15 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+echo "--- Building + deploying backend service"
+pushd ~/form
+git pull
+go build -o form_exe
+sudo mv form_exe /opt/form/form_exe
+sudo systemctl restart form.service
+popd
+
+echo "--- Deploying FE code"
 sudo rm -rf /var/www/form
-sudo mv ~/form /var/www/form
+sudo mv ~/form_fe /var/www/form
 sudo systemctl reload nginx.service
